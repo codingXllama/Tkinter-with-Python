5e1ab71555ca75709f2e8f36d7b899e3e764863d
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-21 12:22:15 -0600

    Created title

--- BasicApps/buttonWindow.py
+++ BasicApps/buttonWindow.py
@@ -17,6 +17,10 @@ def btn2_Action():
     print("Have a safe journey")
 
 
+# Creating Label
+
+title=Label(window,text="Welcome young one!",font="Arial",padx=10,pady=20)
+title.pack()
 # Creating 2 buttons
 btn1 = Button(window, text="Enter", command=btn1_Action)
 btn2 = Button(window, text="Exit", command=btn2_Action)
@@ -28,6 +32,7 @@ btn2 = Button(window, text="Exit", command=btn2_Action)
 btn1.place(bordermode=OUTSIDE, height=50,
            width=100, x=100, y=80)
 btn2.place(bordermode=INSIDE, height=50, width=100, x=100, y=150)
+
 # imgBtn.Place()
 
 mainloop()
