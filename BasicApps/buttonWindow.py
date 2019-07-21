cc6d01e06fa3a9f3c9c69c46bdce547bfe45c64a
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-21 10:41:18 -0600

    Created basic structure

---
+++ BasicApps/buttonWindow.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")
+
+# Changing the background color of the window
+window.config(background="black")
+
+# Creating the methods that does something when a button calls it
+
+
+def btn1_Action():
+    print("Greeting young one!")
+
+
+def btn2_Action():
+    print("Have a safe journey")
+
+
+# Creating 2 buttons
+btn1 = Button(window, text="Enter", command=btn1_Action)
+btn2 = Button(window, text="Exit", command=btn2_Action)
+
+# img Btn
+# imgBtn = PhotoImage(file="1.jpg")
+# Placing the buttons on the tkinter window
+# btn1.pack()
+btn1.place(bordermode=OUTSIDE, height=50,
+           width=100, x=100, y=80)
+btn2.place(bordermode=INSIDE, height=50, width=100, x=100, y=150)
+# imgBtn.Place()
+
+mainloop()
